73beab3d037ab537d961a6faecb8f9ed68c96a26
Per Sandström <pg.sandstrom@gmail.com>
2020-02-23 12:26:03 +0100

    Cleanup

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  'jsxBracketSameLine': false,
-  'printWidth': 100,
-  'semi': false,
-  'singleQuote': true,
-  'tabWidth': 2,
-  'trailingComma': 'all',
-  'useTabs': false,
+  jsxBracketSameLine: false,
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
 }
