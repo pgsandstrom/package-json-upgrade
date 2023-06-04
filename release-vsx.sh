471f2abf3e3656b3642a4dc2855c15afb5bb3f62
Per Sandström <hello@persandstrom.com>
2023-06-04 20:02:29 +0200

    Fix so release-vsx uses .env.local

--- release-vsx.sh
+++ release-vsx.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-# Add the release token to .env as variable TOKEN. Like this:
+# Add the release token to .env.local as variable TOKEN. Like this:
 # TOKEN=my_token
 
-if [ ! -f .env ]; then
-    echo ".env file not found!"
+if [ ! -f .env.local ]; then
+    echo ".env.local file not found!"
     exit 1
 fi
 
 # ugly hax to read .env file
-export $(grep -v '^#' .env | xargs -d '\n')
+export $(grep -v '^#' .env.local | xargs -d '\n')
 
 DIRTY_VER=$(cat package.json | grep \"version\")
 VER=$(node release-vsx-helper.js $DIRTY_VER)
